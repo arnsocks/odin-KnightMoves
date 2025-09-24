7ad71ed6733ad965a5e6fc2f5418f58466d5b283
James Arn <132308884+arnsocks@users.noreply.github.com>
2025-09-23 20:11:34 -0400

    Prevent the search from returning to the same node twice.

--- knightMoves.js
+++ knightMoves.js
@@ -20,18 +20,15 @@ function knightMoves(start, end) {
   let visited = [];
   let queue = [start];
 
-  let maxMoves = 10;
+  let maxMoves = 20000;
   let j = 1;
-  while (queue.length > 0 && j <= maxMoves) {
+  while (queue.length > 0 && j < maxMoves) {
     console.log("Move #: " + j);
     console.log("Queue: ");
     console.log(queue);
     let current = queue.shift();
     console.log("Current: ");
     console.log(current);
-    console.log("Target: ");
-    console.log(end);
-    console.log(`Match? ${nodeEqual(current, end)}`);
     visited.push(current);
 
     if (nodeEqual(current, end)) {
@@ -45,15 +42,35 @@ function knightMoves(start, end) {
     // add any possible next moves to the queue
     for (let i = 0; i < moves.length; i++) {
       let next = [current[0] + moves[i][0], current[1] + moves[i][1]];
+      let inQueue = false;
+      let inVisited = false;
+
+      // Do not include moves that are out of bounds
       if (next[0] < 0 || next[0] > 7 || next[1] < 0 || next[1] > 7) {
         continue;
-      } else if (visited.includes(next)) {
-        console.log(`Next (${next} was already visited and is excluded)`);
-        continue;
-      } else {
-        newMoves.push(next);
-        queue.push(next);
       }
+
+      // Do not include moves that are to nodes already visited
+      for (let k = 0; k < visited.length; k++) {
+        if (nodeEqual(next, visited[k])) {
+          console.log(`Next (${next}) was already visited and is excluded`);
+          inVisited = true;
+          break;
+        }
+      }
+
+      // Do not include moves that are already in the queue
+      for (let k = 0; k < queue.length; k++) {
+        if (nodeEqual(next, queue[k])) {
+          console.log(
+            `Next (${next}) was already in the queue and is excluded`
+          );
+          inQueue = true;
+          break;
+        }
+      }
+      // newMoves.push(next);
+      if (!inVisited && !inQueue) queue.push(next);
     }
     console.log("Current: ");
     console.log(current);
@@ -62,7 +79,7 @@ function knightMoves(start, end) {
   }
 }
 
-knightMoves([0, 0], [4, 2]);
+knightMoves([0, 0], [7, 7]);
 
 function nodeEqual(arrA, arrB) {
   if (arrA[0] != arrB[0]) return false;
